bccb8301a426ccccfe12975475591020ad477550
olusprogr <olus.main333@gmail.com>
2025-07-04 21:47:49 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'kp';
+
+    code = `
+  const express = require('express')
+  const app = express()
+  const port = 3000
+
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.listen(port, () => {
+    console.log(\`Example app listening on port \${port}\`)
+  })`;
 }
